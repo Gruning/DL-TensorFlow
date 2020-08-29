e84e210ee060da2be258af0ad63667b37bc63f0b
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-28 21:38:57 -0300

    first tensor

--- firstPythonFile.py
+++ firstPythonFile.py
@@ -5,3 +5,11 @@ b = tf.constant(2)
 
 c = a + b 
 d = a * b 
+# vector 1 dimensional
+V1 = tf.constant([1.,2.])
+V2 = tf.constant([3.,4.])
+# matrix 2 d
+M = tf.constant([[1.,2.]])
+N = tf.constant([[1.,2.],[3.,4.]])
+# tensor 3d+
+K = tf.constant([[[1.,2.],[3.,4.]]])

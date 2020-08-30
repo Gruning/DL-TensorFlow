a0a7a443a351166f9c920ff3fe8cbb2648d0f6d0
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-30 15:14:19 -0300

    first tf session

--- firstPythonFile.py
+++ firstPythonFile.py
@@ -13,3 +13,13 @@ M = tf.constant([[1.,2.]])
 N = tf.constant([[1.,2.],[3.,4.]])
 # tensor 3d+
 K = tf.constant([[[1.,2.],[3.,4.]]])
+
+#basic operations
+V3 = V1 + V2 
+M2 = N * M
+
+#matrix multiplicatio 
+NN = tf.matmul(N,N)
+
+#create a TF session 
+sess = tf.Session()
